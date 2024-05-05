efbc95c5699223c22cff1459680299cffaf324ea
Siri P R <siri.pr914@gmail.com>
2024-05-05 12:18:09 +0530

    [longest common prefix] working solution

---
+++ longestcommonprefix/longest_common_prefix.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"dog", "racecar", "car"}))
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+}
+
+func longestCommonPrefix(strs []string) string {
+	var prefix string
+	for i := 0; ; i++ {
+		var currChar string
+		for index, str := range strs {
+			if i < len(str) {
+				if index == 0 {
+					currChar = string(str[i])
+				} else if currChar != string(str[i]) {
+					currChar = ""
+					break
+				}
+			} else {
+				currChar = ""
+				break
+			}
+		}
+		if currChar == "" {
+			break
+		}
+		prefix = prefix + currChar
+	}
+	return prefix
+}
