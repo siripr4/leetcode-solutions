3599d6125001f85b345be14946a6842ba347b53c
Siri P R <siri.pr914@gmail.com>
2024-05-05 11:35:50 +0530

    [roman to integer] iterate backwards to remove buffer

--- romantointeger/roman_to_integer.go
+++ romantointeger/roman_to_integer.go
@@ -8,30 +8,27 @@ func main() {
 }
 
 func romanToInt(s string) int {
-	symbolValues := map[rune]int{
-		'I': 1,
-		'V': 5,
-		'X': 10,
-		'L': 50,
-		'C': 100,
-		'D': 500,
-		'M': 1000,
+	symbolValues := map[string]int{
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
 	}
 
-	buffer := 0
 	previous := 0
 	sum := 0
 
-	for _, char := range s {
-		curr := symbolValues[char]
-		if curr <= previous {
-			sum = sum + previous - buffer
-			buffer = 0
+	for i := len(s) - 1; i >= 0; i-- {
+		curr := symbolValues[string(s[i])]
+		if curr < previous {
+			sum -= curr
 		} else {
-			buffer = buffer + previous
+			sum += curr
 		}
 		previous = curr
 	}
-	sum = sum - buffer + previous
 	return sum
 }
