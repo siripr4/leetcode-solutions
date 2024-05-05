b71cd573898ae9f15a7f0b7e9d487fc9f58d902b
Siri P R <siri.pr914@gmail.com>
2024-05-05 11:27:52 +0530

    [roman to integer] working solution

--- romantointeger/roman_to_integer.go
+++ romantointeger/roman_to_integer.go
@@ -8,35 +8,30 @@ func main() {
 }
 
 func romanToInt(s string) int {
-	symbolValues := map[string]int{
-		"I": 1,
-		"V": 5,
-		"X": 10,
-		"L": 50,
-		"C": 100,
-		"D": 500,
-		"M": 1000,
+	symbolValues := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
 	}
 
 	buffer := 0
 	previous := 0
 	sum := 0
 
-	for index, val := range s {
-		char := string(val)
+	for _, char := range s {
 		curr := symbolValues[char]
-		if index == 0 || curr <= previous {
-			fmt.Print("if ")
-			sum = sum + curr - buffer
+		if curr <= previous {
+			sum = sum + previous - buffer
 			buffer = 0
-			fmt.Printf("previous: %d, curr: %d, buffer: %d, sum: %d\n", previous, curr, buffer, sum)
 		} else {
-			fmt.Print("else ")
 			buffer = buffer + previous
-			// sum = sum + curr
-			fmt.Printf("previous: %d, curr: %d, buffer: %d, sum: %d\n", previous, curr, buffer, sum)
 		}
 		previous = curr
 	}
+	sum = sum - buffer + previous
 	return sum
 }
