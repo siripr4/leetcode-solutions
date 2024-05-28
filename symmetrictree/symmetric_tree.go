699deedd8d47c6d488a083df18dea1baf617ec73
Siri P R <siri.pr914@gmail.com>
2024-05-28 21:49:59 +0530

    [symmetric tree] working solution with recursion

---
+++ symmetrictree/symmetric_tree.go
@@ -0,0 +1,32 @@
+package main
+
+func main() {
+
+}
+
+// TreeNode defines a binary tree node
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return isMirror(root.Left, root.Right)
+}
+
+func isMirror(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	} else if left == nil || right == nil {
+		return false
+	}
+
+	return (left.Val == right.Val) &&
+		isMirror(left.Left, right.Right) &&
+		isMirror(left.Right, right.Left)
+}
