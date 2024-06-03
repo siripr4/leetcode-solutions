dd79ec470352e34e6a56ae334b75d9fc0f655b4d
Siri P R <siri.pr914@gmail.com>
2024-06-03 22:16:32 +0530

    [same tree] working optimised solution

---
+++ sametree/same_tree.go
@@ -0,0 +1,20 @@
+package main
+
+// TreeNode defines a binary tree's structure
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Right) && isSameTree(p.Right, q.Right)
+}
